b0dd50b9fc75334053e33a02ff1a550d7545e7e3
mrudolph <michael.rudolph@open-dpp.de>
2025-03-05 16:35:54 +0100

    fix: Tests for index store

--- src/stores/index.spec.ts
+++ src/stores/index.spec.ts
@@ -2,6 +2,8 @@ import { setActivePinia, createPinia } from "pinia";
 import { vi, expect, it } from "vitest";
 import { useIndexStore } from "./index";
 import apiClient from "../lib/api-client";
+import { waitFor } from "@testing-library/vue";
+import { LAST_SELECTED_ORGANIZATION_ID_KEY } from "../const";
 
 vi.mock("../lib/api-client", () => ({
   default: {
@@ -15,10 +17,21 @@ describe("IndexStore", () => {
     setActivePinia(createPinia());
   });
 
-  it("should update organization id of api client on orga select", () => {
+  it("should update organization id of api client on orga select", async () => {
     const indexStore = useIndexStore();
     const id = "someId";
     indexStore.selectOrganization(id);
-    expect(apiClient.setActiveOrganizationId).toHaveBeenCalled();
+    await waitFor(() =>
+      expect(apiClient.setActiveOrganizationId).toHaveBeenCalledWith(id),
+    );
+  });
+
+  it("should update organization id of api client on startup", async () => {
+    const id = "initialId";
+    localStorage.setItem(LAST_SELECTED_ORGANIZATION_ID_KEY, id);
+    useIndexStore();
+    await waitFor(() =>
+      expect(apiClient.setActiveOrganizationId).toHaveBeenCalledWith(id),
+    );
   });
 });
