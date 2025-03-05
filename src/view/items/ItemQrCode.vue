8c5718a5407820f1f01e1501b6f961c611c0e0ee
mrudolph <michael.rudolph@open-dpp.de>
2025-03-05 17:04:07 +0100

    fix: QR Code view

--- src/view/items/ItemQrCode.vue
+++ src/view/items/ItemQrCode.vue
@@ -4,20 +4,19 @@
 <script lang="ts" setup>
 import { onMounted, ref } from "vue";
 import { useRoute } from "vue-router";
-
-import axiosIns from "../../lib/axios";
 import { VIEW_ROOT_URL } from "../../const";
 import QrCode from "../../components/QrCode.vue";
+import apiClient from "../../lib/api-client";
 
 const route = useRoute();
 const url = ref<string>();
 
 onMounted(async () => {
-  const response = await axiosIns.get(
-    `/models/${route.params.modelId}/items/${route.params.itemId}`,
+  const response = await apiClient.items.getItem(
+    String(route.params.modelId),
+    String(route.params.itemId),
   );
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const item = response.data as any;
+  const item = response.data;
   url.value = `${VIEW_ROOT_URL}/${item.uniqueProductIdentifiers[0].uuid}`;
 });
 </script>
