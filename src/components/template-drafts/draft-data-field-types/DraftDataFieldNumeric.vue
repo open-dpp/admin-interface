0cdd0bce67a195c6bcb930580c9ac345c40db3d4
Florian Bieck <info@florianbieck.com>
2025-08-14 14:55:06 +0200

    feat: improve accessibility and interaction for DraftDataFieldNumeric
    - Add `aria-label` and `tabindex` for better accessibility.
    - Replace `mousedown` event with `click` for user interaction.
    - Add keyboard support for Enter and Space keys.

--- src/components/template-drafts/draft-data-field-types/DraftDataFieldNumeric.vue
+++ src/components/template-drafts/draft-data-field-types/DraftDataFieldNumeric.vue
@@ -1,10 +1,15 @@
 <template>
   <input
     :placeholder="dataField.name"
+    :aria-label="dataField.name"
+    role="button"
+    tabindex="0"
     class="block w-full cursor-pointer select-none rounded-md border-0 py-1.5 text-gray-900 shadow-xs ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 sm:text-sm sm:leading-6"
     readonly
     type="text"
-    @mousedown.prevent="emits('clicked')"
+    @click="emits('clicked')"
+    @keydown.enter.prevent="emits('clicked')"
+    @keydown.space.prevent="emits('clicked')"
   />
 </template>
 <script lang="ts" setup>
