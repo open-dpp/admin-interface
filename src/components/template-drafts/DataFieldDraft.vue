b7c446724856912d5bc9db6feab40bf47d569a40
Florian Bieck <info@florianbieck.com>
2025-08-19 11:27:05 +0200

    ### Refactored FileField component with improved upload, download, and fetch functionality ```markdown # Updated FileField component with improved upload and download logic # Refactored media upload, download, and fetch functionality using Pinia store # Added error handling and logging for better user experience ``` This commit refactors the `FileField` component to improve its upload and download logic. It utilizes a Pinia store for managing state and adds features like error handling and logging to enhance the overall user experience. The changes include updating media service URL paths, correcting file upload URLs, and reformatting import statements for better readability.

--- src/components/template-drafts/DataFieldDraft.vue
+++ src/components/template-drafts/DataFieldDraft.vue
@@ -1,22 +1,14 @@
 <template>
   <div>
-    <DraftDataFieldText
-      v-if="props.dataField.type === DataFieldType.TEXT_FIELD"
-      :data-field="props.dataField"
-      @clicked="onClicked"
-    />
-    <DraftDataFieldProductPassportLink
-      v-else-if="props.dataField.type === DataFieldType.PRODUCT_PASSPORT_LINK"
-      :data-field="props.dataField"
-      @clicked="onClicked"
-    />
-    <DraftDataFieldNumeric
-      v-else-if="props.dataField.type === DataFieldType.NUMERIC_FIELD"
-      :data-field="props.dataField"
-      @clicked="onClicked"
-    />
-    <DraftDataFieldFile
-      v-else-if="props.dataField.type === DataFieldType.FILE_FIELD"
+    <DraftDataFieldGeneric
+      v-if="
+        [
+          DataFieldType.TEXT_FIELD,
+          DataFieldType.PRODUCT_PASSPORT_LINK,
+          DataFieldType.NUMERIC_FIELD,
+          DataFieldType.FILE_FIELD,
+        ].includes(props.dataField.type)
+      "
       :data-field="props.dataField"
       @clicked="onClicked"
     />
@@ -30,11 +22,8 @@ import {
   SidebarContentType,
   useDraftSidebarStore,
 } from "../../stores/draftSidebar";
-import DraftDataFieldNumeric from "./draft-data-field-types/DraftDataFieldNumeric.vue";
-import DraftDataFieldText from "./draft-data-field-types/DraftDataFieldText.vue";
-import DraftDataFieldProductPassportLink from "./draft-data-field-types/DraftDataFieldProductPassportLink.vue";
 import DraftDataFieldUnsupported from "./draft-data-field-types/DraftDataFieldUnsupported.vue";
-import DraftDataFieldFile from "./draft-data-field-types/DraftDataFieldFile.vue";
+import DraftDataFieldGeneric from "./draft-data-field-types/DraftDataFieldGeneric.vue";
 
 const props = defineProps<{ dataField: DataFieldDto }>();
 
